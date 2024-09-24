c8942838789eff6d918877d0f24f36145b50de1e
mrbrdo <mrbrdo@gmail.com>
2024-09-24 13:19:02 +0200

    fix for Zeitwerk

--- lib/spree_stock_status/engine.rb
+++ lib/spree_stock_status/engine.rb
@@ -11,9 +11,18 @@ module SpreeStockStatus
       g.test_framework :rspec
     end
 
+    initializer "let the main autoloader ignore this engine's overrides" do
+      overrides = root.join("app/overrides")
+      Rails.autoloaders.main.ignore(overrides)
+    end
+
     def self.activate
       Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_decorator*.rb')) do |c|
         Rails.configuration.cache_classes ? require(c) : load(c)
+      end
+      
+      Dir.glob(File.join(File.dirname(__FILE__), "../../app/overrides/*.rb")) do |c|
+        load(c)
       end
     end
 
